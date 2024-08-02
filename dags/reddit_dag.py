542853c58aa33d435f61bb7e8762e3b2e971195d
dungvann1806 <102403216+dungvann1806@users.noreply.github.com>
2024-08-02 14:16:11 +0700

    RedditPipeline : 2

--- dags/reddit_dag.py
+++ dags/reddit_dag.py
@@ -1,35 +1,24 @@
-from airflow import DAG
-from airflow.operators.python import PythonOperator
-from datetime import date, datetime
 import os
 import sys
-# Thêm đường dẫn thư mục hiện tại vào sys.path
-sys.path.insert(0, os.path.dirname(os.path.abspath(__file__)))
+from datetime import datetime
+from airflow import DAG
 
-default_args = {
-    'owner' : 'ClarkSonTech',
-    'start_date' : datetime.now(),
-    
+sys.path.insert(0 , os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+defaults_dag = {
+    'owner': 'ClarkSon',
+    'start_date': datetime.now()
 }
-file_postfix = datetime.now().strftime("%Y%m%d")
 
+file_postfix = datetime.now().strftime('%Y%m%d')
 dag = DAG(
-    dag_id='etl_reddit_pipeline',
-    default_args=default_args,
-    schedule='@daily',
-    catchup=False,
-    tags = ['reddit', 'etl' , 'pipeline']
+    dag_id = 'etl_reddit_pipeline',
+    default_args= defaults_dag , 
+    schedule_interval= '@daily',
+    catchup= False,
+    tags=['reddit' , 'etl' , 'pipeline']
 )
-#extract from reddit api
-extract = PythonOperator(
-    task_id = 'reddit_extraction',
-    python_callable = reddit_pipeline,
-    op_kwargs={
-        'file_name' : f'reddit_{file_postfix}',
-        'subreddit' :'dataengineering',
-        'time_filter' : 'day',
-        'limit' : 100
-    }
-)
+
+
 
 
--- pipelines/reddit_pipeline.py
+++ pipelines/reddit_pipeline.py
@@ -1,7 +1,7 @@
 
 def reddit_pipeline(file_name: str, subreddit: str, timefilter: str = 'day', limit: int = None): 
     #connecting to reddit instance
-    
+    isinstance = conn
     #extraction 
     #tranformation 
     # load data from reddit
